ce9ba995da7ae534d4c8025c19f99ab5c5ed085f
tianchai4744 <tianchai4744@gmail.com>
2026-02-07 12:34:29 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./pages/**/*.html",
+    "./components/**/*.html",
+    "./admin/**/*.html",
+    "./js/**/*.js",
+    "./admin/scripts/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
