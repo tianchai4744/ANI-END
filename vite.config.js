2f75e7208fee19148bcce3b7903ab4d07966d145
tianchai4744 <tianchai4744@gmail.com>
2026-02-07 12:34:16 +0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        bookmarks: resolve(__dirname, 'pages/bookmarks.html'),
+        grid: resolve(__dirname, 'pages/grid.html'),
+        history: resolve(__dirname, 'pages/history.html'),
+        player: resolve(__dirname, 'pages/player.html'),
+        profile: resolve(__dirname, 'pages/profile.html'),
+        admin: resolve(__dirname, 'admin/admin.html')
+      },
+    },
+  },
+  server: {
+    open: true
+  }
+});
